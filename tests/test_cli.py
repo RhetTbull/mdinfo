1f72faa6b9259cff7691b9375e25be0663b1ef46
Rhet Turnbull <rturnbull@gmail.com>
2023-03-16 20:32:58 -0700

    Added tests for CLI

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,10 +1,10 @@
 """Test mdinfo CLI"""
 
+import json
 import pathlib
 import re
 from os import stat, utime
 from shutil import copyfile
-import json
 
 import pytest
 from click.testing import CliRunner
@@ -22,7 +22,7 @@ def copy_file(source, target):
 
 
 @pytest.fixture(scope="function")
-def source(tmpdir_factory):
+def source(tmpdir_factory) -> pathlib.Path:
     cwd = pathlib.Path.cwd()
     tmpdir = pathlib.Path(tmpdir_factory.mktemp("data"))
     copy_file(cwd / TEST_IMAGE_1, tmpdir / pathlib.Path(TEST_IMAGE_1).name)
@@ -33,15 +33,36 @@ def source(tmpdir_factory):
 
 
 @pytest.fixture(scope="function")
-def target(tmpdir_factory):
+def target(tmpdir_factory) -> pathlib.Path:
     return pathlib.Path(tmpdir_factory.mktemp("target"))
 
 
-def test_cli_print(source, target):
+def test_cli_print(source: pathlib.Path, target: pathlib.Path):
     """Test CLI with -p/--print"""
     from mdinfo.cli import cli
 
-    source_files = list(source.glob("*"))
+    source_files = sorted(list(source.glob("*")))
+
+    runner = CliRunner()
+    result = runner.invoke(
+        cli,
+        [
+            "--print",
+            "{filepath.name}",
+            *[str(p) for p in source_files],
+        ],
+    )
+    assert result.exit_code == 0
+    assert (
+        result.output == "\n".join([f"{p.name}: {p.name}" for p in source_files]) + "\n"
+    )
+
+
+def test_cli_print_no_filename(source: pathlib.Path, target: pathlib.Path):
+    """Test CLI with -p/--print with --no-filename"""
+    from mdinfo.cli import cli
+
+    source_files = sorted(list(source.glob("*")))
 
     runner = CliRunner()
     result = runner.invoke(
@@ -57,11 +78,37 @@ def test_cli_print(source, target):
     assert result.output == "\n".join([p.name for p in source_files]) + "\n"
 
 
-def test_cli_csv(source, target):
+def test_cli_print_null_separator(source: pathlib.Path, target: pathlib.Path):
+    """Test CLI with -p/--print and --null-separator"""
+    from mdinfo.cli import cli
+
+    source_files = sorted(list(source.glob("*")))
+
+    runner = CliRunner()
+    result = runner.invoke(
+        cli,
+        [
+            "--print",
+            "{filepath.name}",
+            "--print",
+            "{size}",
+            "--no-filename",
+            "--null-separator",
+            *[str(p) for p in source_files],
+        ],
+    )
+    assert result.exit_code == 0
+    assert (
+        result.output
+        == "\n".join(f"{p.name}\0{p.stat().st_size}" for p in source_files) + "\n"
+    )
+
+
+def test_cli_csv(source: pathlib.Path, target: pathlib.Path):
     """Test CLI with --csv"""
     from mdinfo.cli import cli
 
-    source_files = list(source.glob("*"))
+    source_files = sorted(list(source.glob("*")))
 
     runner = CliRunner()
     result = runner.invoke(
@@ -87,11 +134,36 @@ def test_cli_csv(source, target):
     ]
 
 
-def test_cli_json(source, target):
+def test_cli_json(source: pathlib.Path, target: pathlib.Path):
     """Test CLI with --json"""
     from mdinfo.cli import cli
 
-    source_files = list(source.glob("*"))
+    source_file = sorted(list(source.glob("*")))[0]
+    runner = CliRunner()
+
+    result = runner.invoke(
+        cli,
+        [
+            "--print",
+            "file:{filepath.name}",
+            "-p",
+            "{size}",
+            "--no-filename",
+            "--json",
+            str(source_file),
+        ],
+    )
+    assert result.exit_code == 0
+    got = json.loads(result.output)
+    expected = {"file": "flowers.jpeg", "size": "3449684"}
+    assert got == expected
+
+
+def test_cli_json_array(source: pathlib.Path, target: pathlib.Path):
+    """Test CLI with --json with --array"""
+    from mdinfo.cli import cli
+
+    source_files = sorted(list(source.glob("*")))
 
     runner = CliRunner()
     result = runner.invoke(
